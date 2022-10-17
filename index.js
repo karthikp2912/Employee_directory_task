2114843323c6d8bbaec8d624347e13118761ba41
karthikp2912 <113369202+karthikp2912@users.noreply.github.com>
2022-10-17 10:46:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
-class empDataClass{
-   static ImgSrc="images/profile-pic.jpg";
-   FirstName;LastName;Email;JobTitle;Office;Department;PhoneNumber;SkypeID;
-   constructor(FirstName,LastName,Email,JobTitle,Office,Department,PhoneNumber,SkypeID)
-   {
-      this.FirstName=FirstName;
-      this.LastName=LastName;
-      this.Email=Email;
-      this.JobTitle=JobTitle;
-      this.Office=Office;
-      this.Department=Department;
-      this.PhoneNumber=PhoneNumber;
-      this.SkypeID=SkypeID;   
-   }
-}
-
-let employeeDetails=[];
-employeeDetails.push(new empDataClass("Karthik", "varma", "karthik@technovert.com","BI developer","Seattle","Human Resources",8978803288,"karthik2912"));
-employeeDetails.push(new empDataClass("Sai","Krishna","saiki@gmail.com","Mobile Developer","Seattle","IT", 6256254567,"krishna846"));
-employeeDetails.push(new empDataClass("Ajay", "Ravada", "ajay@technovert.com",
-"Business Analyst","India","MD",9836522448,"ajay843",));
-employeeDetails.push(new empDataClass("Shiva", "Prasad", "shiva@technovert.com","UX Designer","India","IT",9876265564,"shiva123",));
-employeeDetails.push(new empDataClass("Bhaskar", "Raj", "bhaskar@technovert.com","Web Developer","India","IT",9845675438,"raj233",));
-employeeDetails.push(new empDataClass("Mahesh", "Reddy", "mahesh@technovert.com","Web Developer","India","IT",9876345477,"mahesh9687",));
-employeeDetails.push(new empDataClass("Ravi", "Kiran", "ravi@technovert.com","Web Developer","India","MD",9876345448,"ravi123",))
-employeeDetails.push(new empDataClass("Ashok", "Kumar", "ashok@technovert.com","UX Designer","Seattle","IT",6735673768,"ashok234",))
+const ImgSrc="images/profile-pic.jpg";
+var xhReq = new XMLHttpRequest();
+xhReq.open("GET", "./employeeDetails.json", false);
+xhReq.send(null);
+let employeeDetails = JSON.parse(xhReq.responseText);
 
 function displayLetters(){
    var letters=document.getElementById("lettersContainer");
@@ -38,8 +17,8 @@ function displayLetters(){
 function displayLeftFilters(filterId,property){
    var filterList=document.getElementById(filterId);
    var text="";
-   var propertyArray=employeeDetails.map(emp => emp[property]).filter((v,i,a)=>a.indexOf(v)==i);
-   propertyArray.forEach(ele=>{
+   var Array=employeeDetails.map(emp => emp[property]).filter((v,i,a)=>a.indexOf(v)==i);
+   Array.forEach(ele=>{
       var count=employeeDetails.filter(_=>_[property]==ele).length;
       text+=`<input class="${property}" name="${property}" value="${ele}" type="checkbox" id="${ele}">
       <label value="${ele}" name="${property}" for="${ele}">
@@ -49,20 +28,35 @@ function displayLeftFilters(filterId,property){
    filterList.innerHTML=text;
 }
 
-function displayEmployeeDetails(id){
-   var ele=employeeDetails.filter(_=>_.Email==id);
-   document.getElementById("firstName").value= ele[0].FirstName;
-   //Not yet completed
-   openForm();
+function displaydropdown(){
+   var filterContainer=document.getElementById("filterBy");
+   var temp="";
+   Object.keys(employeeDetails[0]).forEach(element => {
+      temp+=`<option value="${element}">${element}</option>`
+   });
+   filterContainer.innerHTML+=temp;
 }
 
-function displayEmployeeCards( arrayofcards){
+function displayEmployeeDetails(id){
+   var ele=employeeDetails.find(_=>_.Email==id);
+   document.getElementById("FormFirstName").value= ele.FirstName;
+   document.getElementById("FormLastName").value= ele.LastName;
+   document.getElementById("FormEmail").value= ele.Email;
+   document.getElementById("FormJobTitle").value= ele.JobTitle;
+   document.getElementById("FormOffice").value= ele.Office;
+   document.getElementById("FormDepartment").value= ele.Department;
+   document.getElementById("FormPhoneNumber").value= ele.PhoneNumber;
+   document.getElementById("FormSkypeID").value= ele.SkypeID;
+   openFormToChange();
+}
+
+function displayEmployeeCards(arrayofcards){
    var parentDiv=document.getElementById("employeeContainer");
    var temp="";
    for(var i=0;i<arrayofcards.length;i++)
    { 
       temp=temp+`<div class="emp-card" id=${arrayofcards[i].Email} onclick="displayEmployeeDetails(id)">
-      <img src=${empDataClass.ImgSrc}>
+      <img src=${ImgSrc}>
          <div class="detailsDiv"> 
             <p>${arrayofcards[i].FirstName} ${arrayofcards[i].LastName}</p>
             <p>${arrayofcards[i].JobTitle}</p>
@@ -81,7 +75,6 @@ function displayEmployeeCards( arrayofcards){
 }
 
 let selectedItems={}, AfterdepFilter=[], AfterjobFilter=[], AfteroffFilter=[], AfterLetterFilter=[];
-
 function applyFilters(){
    selectedItems={};
    AfterdepFilter=[];
@@ -95,7 +88,7 @@ function applyFilters(){
       }
       selectedItems[element.className].push(element.value);
    });
-   for(p in selectedItems){
+   for(var p in selectedItems){
       fillArrays(p);  
    }
 }
@@ -116,21 +109,21 @@ function fillArrays(p){
 }
 
 let afterSearchFilter=[];
-function searchkey(){
+function searchKey(){
    var filterBy = document.getElementById('filterBy');
    var property = filterBy.options[filterBy.selectedIndex].value;
    var searchValue = String(document.getElementById("search").value);
    afterSearchFilter=[];
    try{
-         for(var i in employeeDetails){
-            if(i[property].includes(searchValue)){
-               afterSearchFilter.push(i.Email)
-            }
+      employeeDetails.forEach(i => {
+         if(i[property].includes(searchValue)){
+            afterSearchFilter.push(i.Email)
          }
-         if(afterSearchFilter.length==0){
-            document.getElementById("employeeContainer").innerHTML=`<p style="opacity:0.7;font-size:13px;text-align:center">"No data found"</p>`;
-         }
-         else merge();
+      });
+      if(afterSearchFilter.length==0){
+         document.getElementById("employeeContainer").innerHTML=`<p style="opacity:0.7;font-size:13px;text-align:center">"No data found"</p>`;
+      }
+      else merge();
    }
    catch(err){
       alert("select Preffered Name first");
@@ -166,13 +159,13 @@ function merge(){
    else{
       var finalMerge=intersection(data);
       finalRender=[];
-      for( var i in finalMerge){
-         for(var j in employeeDetails){
+      finalMerge.forEach(i => {
+         employeeDetails.forEach(j => {
             if(j.Email==i){
                finalRender.push(j);
             }
-         }
-      }
+         });
+      });
       if(finalRender.length==0){
          document.getElementById("employeeContainer").innerHTML=`<p style="opacity:0.7;font-size:13px;text-align:center">"No data found"</p>`;
       }
@@ -182,15 +175,31 @@ function merge(){
 
 function clearContents(){
    document.getElementById("search").value="";
-   searchkey();
+   searchKey();
 }
 
-function openForm(){
+function openFormToAdd(){
    document.getElementById("myForm").style.display="block";
+   document.getElementById("saveChangesBtn").style.display="none";
+   document.getElementById("closeBtn").style.display="none";
 }
 
 function closeForm(){
    document.getElementById("myForm").style.display="none";
+   document.getElementById("saveChangesBtn").style.display="block";
+   document.getElementById("closeBtn").style.display="block";
+}
+
+function openFormToChange(){
+   document.getElementById("myForm").style.display="block";
+   document.getElementById("submitBtn").style.display="none";
+   document.getElementById("cancelBtn").style.display="none";
+}
+
+function closeFormAfterChanges(){
+   document.getElementById("myForm").style.display="none";
+   document.getElementById("submitBtn").style.display="block";
+   document.getElementById("cancelBtn").style.display="block";
 }
 
 function saveEmpData(event){
@@ -208,32 +217,39 @@ function saveEmpData(event){
          alert("Name is too short. please fill a valid name!");
          return false;
       }
-      employeeDetails.push(formDataobj);
-      alert("Employee added");
+      var ele=employeeDetails.find(_=>_.Email==formDataobj.Email);
+      if(ele==undefined){
+         employeeDetails.push(formDataobj);
+         alert("Employee added");
+      }
+      else{
+         Object.assign(ele, formDataobj);
+         alert("Employee Details saved");
+      }
       form.reset();
       closeForm();
       displayEmployeeCards(employeeDetails);
-      displayLeftFilters("departmentList","Department",);
-      displayLeftFilters("officesList","Office",);
-      displayLeftFilters("jobTitleList","JobTitle",);
+      displayLeftFilters("departmentList","Department");
+      displayLeftFilters("officesList","Office");
+      displayLeftFilters("jobTitleList","JobTitle");
       displayLetters();
 }
 
 window.onload = function(){
    displayLetters();
    displayEmployeeCards(employeeDetails);
+   displaydropdown();
    displayLeftFilters("departmentList","Department");
    displayLeftFilters("officesList","Office");
    displayLeftFilters("jobTitleList","JobTitle");
    document.getElementById("clearBtn").onclick=function(){clearContents()};
-   document.getElementById("addEmpBtn").onclick= function(){openForm()};
+   document.getElementById("addEmpBtn").onclick= function(){openFormToAdd()};
    document.getElementById("cancelBtn").onclick=function(){closeForm()};
+   document.getElementById("closeBtn").onclick=function(){closeFormAfterChanges()};
    document.getElementById("form").addEventListener('submit', saveEmpData);
    document.getElementById("lettersContainer").onchange=function(){applyFilters();merge();};
    document.getElementById("departmentList").onchange=function(){applyFilters();merge();};
    document.getElementById("officesList").onchange=function(){applyFilters();merge();};
    document.getElementById("jobTitleList").onchange=function(){applyFilters();merge();};
-   document.getElementById("search").oninput=function(){searchkey()};
+   document.getElementById("search").oninput=function(){searchKey()};
 }
-
-
